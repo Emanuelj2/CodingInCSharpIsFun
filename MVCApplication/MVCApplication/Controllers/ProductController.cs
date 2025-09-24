b77cfdf8078a3cbb9313816bbcb047349b4d50a0
Emanuel Jose <emanueljose0522@hotmail.com>
2025-09-24 18:00:52 -0400

    MVC introduction for beginners

---
+++ MVCApplication/MVCApplication/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Data;
+using MVCApplication.Models;
+
+namespace MVCApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _db.Products.ToList();
+            return View(objProductList);
+        }
+    }
+}
---
+++ MVCApplication/MVCApplication/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+    }
+}
