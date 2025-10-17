040c72f21f054143b9d7b6d5325b7d54d5bb6441
Emanuel Jose <emanueljose0522@hotmail.com>
2025-10-17 11:08:52 -0400

    file for string operations (manual)

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,96 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main()
+    {
+        string str1 = "Hello, ";
+        string str2 = "World!";
+
+        Console.WriteLine("Concatenating two strings without using built-in methods:");
+        string concatenated = concatStrings(str1, str2);
+        Console.WriteLine($"{concatenated}\n");
+
+        Console.WriteLine("Reversing a string without using built-in methods:");
+        string reversed = reverseString(str2);
+        Console.WriteLine($"Original word: {str2}, Reversed: {reversed}\n");
+
+        char targetChar = 'l';
+        Console.WriteLine($"Counting occurrences of character '{targetChar}' in string \"{str1}\":");
+        int count = letterCount(str1, targetChar);
+        Console.WriteLine($"The character '{targetChar}' appears {count} times in \"{str1}\".");
+
+
+
+
+    }
+
+    public static string concatStrings(string str1, string str2)
+    {
+
+        string result = "";
+
+        for (int i = 0; i < str1.Length; i++)
+        {
+            result += str1[i];
+        }
+
+        for(int j = 0; j < str2.Length; j++)
+        {
+            result += str2[j];
+        }
+
+        return result;
+
+    }
+
+    public static string reverseString(string str)
+    {
+        string result = "";
+
+        for(int i = str.Length -1; i >= 0; i--)
+        {
+            result += str[i];
+        }
+
+        return result;
+    }
+
+    public static int letterCount(string str, char target)
+    {
+        int count = 0;
+        for(int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == target)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static bool isPalindrome(string str)
+    {
+        int left = 0;
+        int right = str.Length - 1;
+
+        while (left < right)
+        {
+            if (str[left] != str[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
+
+}
+
+
---
+++ c_code/c_code/cCode.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NPROC 4
+
+struct proc {
+	int pid;                // Process ID
+	char name[16];
+	char name[16];         // Process name
+};
+
+struct proc proc_table[NPROC]; // Process table
+struct proc* init_proc;      // Initial process pointer
+
+int main() {
+	init_proc = &proc_table[0]; // Point to the first process
+	init_proc->pid = 1;        // Set PID to 1
+	strcpy(init_proc->name, "init");
+
+	printf("Process ID: %d, Name: %s\n", init_proc->pid, init_proc->name);
+	return 0;
+
+}
